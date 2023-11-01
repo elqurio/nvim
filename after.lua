dafa6f3be5fee72aff2b10160dd1bb8517c1ae25
lkurija <lkurija@lkurijas-Mac-Studio.local>
2023-11-01 11:16:24 +0100

    More files

---
+++ after.lua
@@ -0,0 +1,42 @@
+-- harpoon
+--
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
+-- fugitive
+--
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+-- telescope
+--
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+
+-- lsp
+--
+local lsp_zero = require("lsp-zero")
+
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({ buffer = bufnr })
+end)
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "pyright",
+        "lua_ls",
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
