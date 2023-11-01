9708002ec2ce0b31505b3e62514f6e66a7b8e2ce
lkurija <lkurija@lkurijas-Mac-Studio.local>
2023-11-01 09:40:16 +0100

    Reimplementation

--- init.lua
+++ init.lua
@@ -1,6 +1,76 @@
-require("config.settings");
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
 
-require("config.lazy");
+vim.opt.rtp:prepend(lazypath)
 
-require("config.lspconfig")
+require("settings")
 
+require("lazy").setup({
+    {
+        "Mofiqul/vscode.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme vscode]])
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup(
+                {
+                    ensure_installed = {
+                        "bash",
+                        "html",
+                        "json",
+                        "lua",
+                        "markdown",
+                        "python",
+                        "vim",
+                        "yaml",
+                    },
+                    sync_install = false,
+                    indent = { enable = true },
+                    highlight = { enable = true },
+                }
+            )
+        end
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "ThePrimeagen/harpoon",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    }, -- LSP-ZERO setup
+    {
+        "VonHeikemen/lsp-zero.nvim", 
+        branch = "v3.x",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip", 
+        }
+    },
+})
+
+require("after")
--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-r = requests.get("https://google.com")
-
-print(r)
-
